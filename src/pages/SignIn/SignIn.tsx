bfe67edc29076fa3a8dc1ebeca976a6abbf741d8
Agent Ebimene <ebimeneagent@gmail.com>
2022-12-19 21:25:39 +0100

    Add up Signin Page to application

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,17 +1,50 @@
+import { Link } from 'react-router-dom';
+
+import googleIcon from '../../assets/images/Google__G__Logo 1.png';
+import facebookIcon from '../../assets/images/1260673-removebg-preview 1.png';
+
 const SignIn: React.FC = () => {
   return (
     <div className='sign-in'>
-      <h1>Login NAPPR</h1>
-      <form action='' className='login-form'>
+      <h1 className='form-header'>Login to NAPPR</h1>
+      <form action='' className='form login-form'>
         <div className='form-control'>
           <label htmlFor='email'>Email</label>
-          <input type='text' placeholder='abcde@gmail.com' name='email' />
+          <div className='input-container'>
+            <input type='text' placeholder='abcde@gmail.com' name='email' />
+          </div>
         </div>
-        <div className='form-control'>
+        <div className='form-control password'>
           <label htmlFor='email'>Password</label>
-          <input type='text' placeholder='*******' name='email' />
-          <p>Forget Password</p>
+          <div className='input-container'>
+            <input type='text' placeholder='*******' name='email' />
+          </div>
+          <p>
+            <Link to='signup'>Forget Password</Link>{' '}
+          </p>
         </div>
+        <p className='form-text'>Or</p>
+        <div className='other-login-method facebook'>
+          <div className='method-icon'>
+            <img src={googleIcon} alt='' />
+          </div>
+          <p>Continue with Google</p>
+        </div>
+        <div className='other-login-method facebook'>
+          <div className='method-icon'>
+            <img src={facebookIcon} alt='' />
+          </div>
+          <p>Continue with Facebook</p>
+        </div>
+        <button type='submit' className='submit-btn'>
+          Login
+        </button>
+        <p>
+          Do not have an account yet?{' '}
+          <Link to='' className='signup-btn'>
+            Signup
+          </Link>
+        </p>
       </form>
     </div>
   );
