a21b473456f9fc7e6e15ce895127be778c848cbb
Agent Ebimene <ebimeneagent@gmail.com>
2022-12-22 18:42:42 +0100

    Add up side menu to app

--- src/components/NapprMenu/NapprMenu.tsx
+++ src/components/NapprMenu/NapprMenu.tsx
@@ -6,22 +6,43 @@ import companyLogo from '../../assets/images/image 1.png';
 const NapprMenu = () => {
   return (
     <section className='nappr-menu'>
-      <article className='desktop-menu'>
-        <span>
-          <i className='fa fa-bars' aria-hidden='true'></i>
-        </span>
-      </article>
+      <article className='desktop-menu'></article>
       <article className='mobile-menu'>
-        <div className='menu-bar'></div>
-        <div className='company-logo'>
-          <img src={companyLogo} alt='' />
+        <div className='mobile-menu-header'>
+          {/* <div className='menu-bar'>
+            <span>
+              <i className='fa fa-bars' aria-hidden='true'></i>
+            </span>
+          </div> */}
+          <div className='company-logo'>
+            <img src={companyLogo} alt='' />
+          </div>
         </div>
+        <div className='divider'></div>
         <div className='mobile-menu-list'>
-          <Link to=''>About Us</Link>
-          <Link to=''>Cancellation Policy</Link>
-          <Link to=''>Hosting Resources</Link>
-          <Link to=''>Health and Safety Updates </Link>
-          <Link to=''>Help and Support</Link>
+          <Link to='' className='mobile-menu-item'>
+            About Us
+          </Link>
+          <Link to='' className='mobile-menu-item'>
+            Cancellation Policy
+          </Link>
+          <Link to='' className='mobile-menu-item'>
+            Hosting Resources
+          </Link>
+          <Link to='' className='mobile-menu-item'>
+            Health and Safety Updates{' '}
+          </Link>
+          <Link to='' className='mobile-menu-item'>
+            Help and Support
+          </Link>
+        </div>
+        <div className='mobile-menu-footer'>
+          <Link to='' className='mobile-menu-item'>
+            Terms and Conditions
+          </Link>
+          <Link to='' className='mobile-menu-item'>
+            Privacy Policy
+          </Link>
         </div>
       </article>
     </section>
