53d2f4fd9d9647aad78896c1ea0590e33bb6037a
Agent Ebimene <ebimeneagent@gmail.com>
2022-12-27 11:55:38 +0100

    Add up Payment Method to nappr

--- src/pages/PaymentMethod/PaymentMethod.tsx
+++ src/pages/PaymentMethod/PaymentMethod.tsx
@@ -1,20 +1,29 @@
 import './paymentMethod.css';
 import Button from '../../components/Button/Button';
 import BackButton from '../../components/Button/BackButton';
+import masterCardIcon from '../../assets/images/mastercard-logo-mastercard-logo-png-vector-download-19-removebg-preview 1.png';
 const PaymentMethod: React.FC = () => {
   return (
-    <div className='payment-method'>
+    <>
       <BackButton page='/home' />
-      <article className='payment-header'>
-        <h2>Payment Methods </h2>
-        <button type='button'>+</button>
-      </article>
-      <article className='payment-methods'>
-        <div className='card-img'></div>
-        <div className='card-details'></div>
-        <Button text='Remove' />
-      </article>
-    </div>
+      <div className='payment-method'>
+        <article className='payment-header'>
+          <h1>Payment Methods </h1>
+          <div>
+            <button type='button'>+</button>
+          </div>
+        </article>
+        <article className='payment-methods'>
+          <div className='card-details'>
+            <img src={masterCardIcon} alt='' />
+            <div>
+              <p>**********3572</p>
+            </div>
+          </div>
+          <Button text='Remove' />
+        </article>
+      </div>
+    </>
   );
 };
 
