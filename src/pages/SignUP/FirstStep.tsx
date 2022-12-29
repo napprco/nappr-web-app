81c6ef0e79f3b9ab1771291c95a3e103103bf65b
Agent Ebimene <ebimeneagent@gmail.com>
2022-12-29 14:05:34 +0100

    Add up first step of signup form

---
+++ src/pages/SignUP/FirstStep.tsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+
+import googleIcon from '../../assets/images/Google__G__Logo 1.png';
+import facebookIcon from '../../assets/images/1260673-removebg-preview 1.png';
+
+const FirstStep: React.FC = () => {
+  return (
+    <div>
+      <form action='' className='form signup-form'>
+        <div className='form-control'>
+          <label htmlFor='email'>Username</label>
+          <div className='input-container'>
+            <input type='text' placeholder='Username' name='email' />
+          </div>
+        </div>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <div className='input-container'>
+            <input type='text' placeholder='abcde@gmail.com' name='email' />
+          </div>
+        </div>
+
+        <p className='form-text'>Or</p>
+        <div className='other-login-method facebook'>
+          <div className='method-icon'>
+            <img src={googleIcon} alt='' />
+          </div>
+          <p>Continue with Google</p>
+        </div>
+        <div className='other-login-method facebook'>
+          <div className='method-icon'>
+            <img src={facebookIcon} alt='' />
+          </div>
+          <p>Continue with Facebook</p>
+        </div>
+        <button type='button' className='submit-btn'>
+          <p className='submit-btn-link'>Continue</p>
+        </button>
+        <p className='form-question'>
+          Already have an account?{' '}
+          <Link to='/signin' className='signup-btn'>
+            Signin
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default FirstStep;
--- src/pages/SignUP/SignUp.tsx
+++ src/pages/SignUP/SignUp.tsx
@@ -1,8 +1,7 @@
 import { Link } from 'react-router-dom';
 
-import googleIcon from '../../assets/images/Google__G__Logo 1.png';
-import facebookIcon from '../../assets/images/1260673-removebg-preview 1.png';
 import BackButton from '../../components/Button/BackButton';
+import FirstStep from './FirstStep';
 
 const SignUp: React.FC = () => {
   return (
@@ -10,45 +9,7 @@ const SignUp: React.FC = () => {
       <BackButton page='/signin' />
       <div className='page sign-up'>
         <h1 className='form-header'>Signup to NAPPR</h1>
-        <form action='' className='form signup-form'>
-          <div className='form-control'>
-            <label htmlFor='email'>Username</label>
-            <div className='input-container'>
-              <input type='text' placeholder='Username' name='email' />
-            </div>
-          </div>
-          <div className='form-control'>
-            <label htmlFor='email'>Email</label>
-            <div className='input-container'>
-              <input type='text' placeholder='abcde@gmail.com' name='email' />
-            </div>
-          </div>
-
-          <p className='form-text'>Or</p>
-          <div className='other-login-method facebook'>
-            <div className='method-icon'>
-              <img src={googleIcon} alt='' />
-            </div>
-            <p>Continue with Google</p>
-          </div>
-          <div className='other-login-method facebook'>
-            <div className='method-icon'>
-              <img src={facebookIcon} alt='' />
-            </div>
-            <p>Continue with Facebook</p>
-          </div>
-          <button type='button' className='submit-btn'>
-            <Link to='/info' className='submit-btn-link'>
-              Sign Up
-            </Link>
-          </button>
-          <p className='form-question'>
-            Already have an account?{' '}
-            <Link to='/signin' className='signup-btn'>
-              Signin
-            </Link>
-          </p>
-        </form>
+        <FirstStep />
       </div>
     </>
   );
